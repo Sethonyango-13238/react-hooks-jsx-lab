14fd9999f18702d026f31fc787189636c468d933
Sethonyango-13238 <onyangosethoyugi@gmail.com>
2024-08-02 06:12:16 +0300

    firstcommit

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return(
+  <div id="about">
+<h2>About Me</h2>
+<p>I love Sophy,I hope She Loves Me back.She makes Me Happy!</p>
+<img src={image} alt="I made this"/>
+  </div>
+  );
 }
 
 export default About;
